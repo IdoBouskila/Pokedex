7d52627a4925a61b20ade441982763d19301b698
Ido Bouskila <idobouskila22@gmail.com>
2022-03-09 14:10:44 +0200

    Fixed evolution img

--- src/components/Modal/data-types/Evolution.js
+++ src/components/Modal/data-types/Evolution.js
@@ -10,22 +10,22 @@ function normalizeEvolutionsData( evolutionChain ) {
     }
     
     const { evolves_to, species } = evolutionChain;
+
     const evolutions = evolves_to.reduce( ( all, evolution ) => {
         return [ ...all, {
             level: evolution.evolution_details[0].min_level,
             currentEvolution: {
                 name: species.name,
-                imageSRC: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${2}.svg`,
+                imageSRC: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${species.url.split("species/").pop().slice(0, -1)}.svg`,
             },
             nextEvolution: {
                 name: evolution.species.name,
-                imageSRC: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${2}.svg`,
+                imageSRC: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${evolution.species.url.split("species/").pop().slice(0, -1)}.svg`,
             }
         }, ...normalizeEvolutionsData(evolution) ];
-        
     }, [] );
 
-    return evolutions;   
+    return evolutions;
 }
 
 const Evolution = ({ id }) => {
@@ -40,23 +40,36 @@ const Evolution = ({ id }) => {
 
     return (
         <>
-            {
-                evolutions.map(evolution => (
-                    <div key={ evolution.currentEvolution.name + '-' + evolution.nextEvolution.name} className="evolution-container">
-                        <div className="current-evolution">
-                            <img src={ evolution.currentEvolution.imageSRC } alt="" />
-                            <span>{ evolution.currentEvolution.name }</span>
-                        </div>    
-                        
-                        <span className="evolution-level">{ evolution.level }</span>
+            <div className="evolution-tab">
+                {
+                    evolutions.map(evolution => (
+                        <div key={ evolution.currentEvolution.name + '-' + evolution.nextEvolution.name} className="evolution-container">
+                            <div className="current-evolution">
+                                <div className="poke-img">
+                                    <div className="pokeball-bg"></div>
+                                    <img src={ evolution.currentEvolution.imageSRC } alt="" />
+                                </div>
+                                
+                                <span>{ evolution.currentEvolution.name }</span>
+                            </div>    
+                            
+                            <div className="level-container">
+                                <span className="evolution-level">{ evolution.level }</span>
+                                <span className="arrow"></span>
+                            </div>
 
-                        <div className="next-evolution">
-                            <img src={ evolution.nextEvolution.imageSRC } alt="" />
-                            <span>{ evolution.nextEvolution.name }</span>
+                            <div className="next-evolution">
+                                <div className="poke-img">
+                                    <div className="pokeball-bg"></div>
+                                    <img src={ evolution.nextEvolution.imageSRC } alt="" />
+                                </div>
+
+                                <span>{ evolution.nextEvolution.name }</span>
+                            </div>
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
+            </div>
         </>
     );
 };
